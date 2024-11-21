036661a167c480e6aab88c87da151cfb59911281
Dadablog2002 <108271998+Dadablog2002@users.noreply.github.com>
2024-11-22 00:33:25 +0200

    Add files via upload

---
+++ site_server/scripts.js
@@ -0,0 +1,5 @@
+// Функція сповіщення після натискання на кнопку оплати
+document.getElementById("pay-button").addEventListener("click", function() {
+    alert("Ви будете перенаправлені на форму оплати Monobank. Не забудьте вказати свій Minecraft нікнейм в коментарі!");
+});
+
