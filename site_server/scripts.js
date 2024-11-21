86a14d7cf1fe94b4a8d0d8ff07ae492106565d11
Dadablog2002 <108271998+Dadablog2002@users.noreply.github.com>
2024-11-22 00:47:06 +0200

    Delete site_server directory

--- site_server/scripts.js
+++
@@ -1,5 +0,0 @@
-// Функція сповіщення після натискання на кнопку оплати
-document.getElementById("pay-button").addEventListener("click", function() {
-    alert("Ви будете перенаправлені на форму оплати Monobank. Не забудьте вказати свій Minecraft нікнейм в коментарі!");
-});
-
