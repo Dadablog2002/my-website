de39c90d4cf5884986aebe233552eb96bf762a15
Dadablog2002 <108271998+Dadablog2002@users.noreply.github.com>
2024-11-22 11:21:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,12 @@ document.getElementById("pay-button").addEventListener("click", function() {
     alert("Ви будете перенаправлені на форму оплати Monobank. Не забудьте вказати свій Minecraft нікнейм в коментарі!");
 });
 
-// Функція для відкриття та закриття випадаючого меню (можна додати для мобільних пристроїв)
-document.querySelector('.dropdown').addEventListener('click', function() {
-    const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
-});
-
+// Функція для відкриття та закриття меню
+function toggleNav() {
+    var sideNav = document.getElementById("side-nav");
+    if (sideNav.style.width === "250px") {
+        sideNav.style.width = "0";
+    } else {
+        sideNav.style.width = "250px";
+    }
+}
